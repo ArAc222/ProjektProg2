0dd2840f65b2cfb36a1023e16ee8c8dad0e3ab22
ArAc222 <105783863+ArAc222@users.noreply.github.com>
2022-05-19 10:14:22 +0200

    Add files via upload

---
+++ projekt/projekt/podatci.h
@@ -0,0 +1,13 @@
+#ifndef DATATYPE_H
+#define DATATYPE_H
+
+typedef struct oruzje 
+{
+	char ime[25];
+	char domet[25];
+	char [50];
+	char brojMobitela[11];
+	int brojPodignutihFilmova;
+}ORUZJE;
+
+#endif
